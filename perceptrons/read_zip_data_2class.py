b8eccedc25b4a724d10fb2ad490c9f4d77388865
Deepak Gurung <deepakgurung.repo@gmail.com>
2015-03-29 22:24:32 -0700

    changed to proper variable name in reading_test_data()

--- perceptrons/read_zip_data_2class.py
+++ perceptrons/read_zip_data_2class.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 import numpy as np
+from sklearn.decomposition import PCA
 
 def read_training_data(label0,label1):
 	with open('../datasets/zip.train') as f:		
@@ -16,20 +18,20 @@ def read_training_data(label0,label1):
 
 def read_testing_data(label0,label1):
 	with open('../datasets/zip.test') as f:
-		trainX = []
-		trainY = []
+		testX = []
+		testY = []
 		for line in f:
 			line = line.split()
 			if line:
 				if int( float(line[0])) == label0 or int( float( line[0]) )== label1 :
 					line = [float(i) for i in line]			
-					trainY.append(int(line[0]))
-					trainX.append(line[1:])
+					testY.append(int(line[0]))
+					testX.append(line[1:])
 
-	return trainX, trainY
+	return testX, testY
 
 if __name__ == '__main__':
 	label1 = 2;	label2 = 3;
 	trainX, trainY = read_training_data(label1, label2)
-	trainX, trainY = read_testing_data(label1, label2)
+	testX, testY = read_testing_data(label1, label2)
 	print 'number of labels: ', len(set(trainY))
